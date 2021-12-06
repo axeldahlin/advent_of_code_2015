67ba97ced7c2a7a9c3e6b1d85d4dc9313372cb54
Axel Dahlin <axdahlin@gmail.com>
2021-12-06 19:54:12 +0100

    add day 2, part 1

---
+++ spec/day2_part1_spec.js
@@ -0,0 +1,10 @@
+describe("getAmountOfWrappingPaper", function() {
+    const getAmountOfWrappingPaper = require('../lib/day2/day2_part1');
+    const fileReader = require('../lib/utils/fileReader');
+    const puzzleInput = fileReader("lib/day2/input.txt").split("\n");;
+    const correctResult = 1606483;
+
+    it("should get correct amount square feet of wrapping paper", function() {
+        expect(getAmountOfWrappingPaper(puzzleInput)).toEqual(correctResult);
+    });
+});
