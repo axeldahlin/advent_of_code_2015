8a2513927fdd2040ce9d4583ba9924bdedb4b233
Axel Dahlin <axdahlin@gmail.com>
2021-12-06 17:02:58 +0100

    add file reader and test for it
    Signed-off-by: Axel Dahlin <axdahlin@gmail.com>

---
+++ spec/fileReader/fileReader_spec.js
@@ -0,0 +1,9 @@
+describe("fileReader", function() {
+    const fileReader = require('../../lib/utils/fileReader');
+    const result = fileReader("/spec/fileReader/input.txt");
+    const expectedResult = "This is an input file for testing!";
+
+    it("should should get text from txt file", function() {
+        expect(result).toEqual(expectedResult);
+    });
+});
